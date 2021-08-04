c4cac1df6e1774b2399e0532a043d93c5493048b
nix24 <82765899+nix24@users.noreply.github.com>
2021-08-03 21:10:27 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,53 @@
+import {
+    update as updateSnake,
+    draw as drawSnake,
+    SNAKE_SPEED,
+
+} from './snake.js'
+
+import {
+    update as updateFood,
+    draw as drawFood,
+} from './food.js'
+
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
+
+function main(currentTime) {
+    if(gameOver) {
+       if(confirm('You lost. Press Ok to restart.')) {
+           window.location = '/'
+       }
+       return
+    }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) {
+        return;
+    }
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+    updateSnake();
+    updateFood();
+    
+}
+
+function draw() {
+    gameBoard.innerHTML='';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+
+}
+
+
